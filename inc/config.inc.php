eb88c5fcccc218f7105ab665755cbac406d9a44f
amey <amey88_m@yahoo.com>
2019-08-29 20:03:11 +0530

    making changes in name

--- inc/config.inc.php
+++ inc/config.inc.php
@@ -1,12 +1,8 @@
 <?php
-
 # set-default root;
-define("root" , $_SERVER['DOCUMENT_ROOT'] . "/project_mynaukri/");
-
-# user-name & password for phpmailer;
+define("root" , $_SERVER['DOCUMENT_ROOT'] . "/project_naukri/");
+# user-name & user-password for phpmailer;
 # consider our default SMTP server - email address is = user;
 define("user", ""); ## insert your business email address;
 define("password", "");  # insert your business email password;
-define("tabTitle", "mynaukri");
-
-?>
+define("tabTitle", "mynaukri"); ?>
